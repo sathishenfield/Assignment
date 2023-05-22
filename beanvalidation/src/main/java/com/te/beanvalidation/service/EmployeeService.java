54773d4157365171eb832c36716d5bc065659e3e
sathishenfield <121010110+sathishenfield@users.noreply.github.com>
2023-05-22 16:10:12 +0530

    Delete beanvalidation directory

--- beanvalidation/src/main/java/com/te/beanvalidation/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.te.beanvalidation.service;
-
-import com.te.beanvalidation.entity.Employee;
-
-public interface EmployeeService {
-	
-	public Employee registerEmployee(Employee employee);
-
-}
