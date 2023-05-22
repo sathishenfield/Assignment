615cf397a1c0c671879492442fabcd90a42a5565
sathishenfield <121010110+sathishenfield@users.noreply.github.com>
2023-05-22 16:12:27 +0530

    Delete steram directory

--- steram/src/main/java/com/te/stream/repository/OrderDAO.java
+++
@@ -1,9 +0,0 @@
-package com.te.stream.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.te.stream.entity.Order;
-
-public interface OrderDAO extends JpaRepository<Order, Long> {
-
-}
--- steram/src/main/java/com/te/stream/repository/ProductDAO.java
+++
@@ -1,9 +0,0 @@
-package com.te.stream.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.te.stream.entity.Product;
-
-public interface ProductDAO extends JpaRepository<Product, Long> {
-
-}
